11fbafac60e586402e0a4d840c81ba8e1173c54d
exyrion <whoaitsjustin@yahoo.com>
2015-04-30 23:45:12 -0700

    Landscape BFS

---
+++ src/landscape/Landscape.java
@@ -0,0 +1,272 @@
+package landscape;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Landscape {
+	
+	// global variables
+	static int rowSize, colSize;
+	static int w, g; // water level, gradient threshold
+	static Point a, b; // points A and B for question 3
+	static int[][] landscape; // 2d array of the whole landscape
+	static int[][] visited; // helper 2d array to keep track of visited points for BFS, 0 = unvisited, 1 = visited
+	static ArrayList<Lake> lakes = new ArrayList<Lake>(); // an arraylist of lakes, where lakes are just an arraylist of points
+		
+	// a point contains row and column indexes, and z value
+	public static class Point{
+		int row, col, elev;
+		Point parent = null;
+		// Point constructors
+		public Point(int r, int c, int z){
+			row = r;
+			col = c;
+			elev = z;
+		}
+		public Point(int r, int c, int z, Point p){
+			row = r;
+			col = c;
+			elev = z;
+			parent = p;
+		}
+		// prints the point's values
+		public void printPoint(){
+			System.out.println("(" + row + ", " + col + "), z=" + elev);
+		}
+	}
+	
+	// a lake contains an ArrayList of points
+	public static class Lake{
+		ArrayList<Point> lake = new ArrayList<Point>();
+		int SA = 0, vol = 0;
+		// adds a point to the lake
+		public void addPoint(Point p){
+			lake.add(p);
+		}
+		// computes surface area of lake
+		public int computeSA(){
+			return lake.size();
+		}		
+		// computes volume of lake
+		public int computeVol(){
+			int volume = 0;
+			for(int i = 0; i < lake.size(); i++){
+				volume += w - lake.get(i).elev;
+			}
+			return volume;
+		}
+		// prints all the points in the lake
+		public void printLake(){
+			for(int i = 0; i < lake.size(); i++){
+				lake.get(i).printPoint();
+			}
+		}
+	}
+	
+	// method to find all the lakes in the landscape
+	public static void findLakes(){
+		for(int i = 0; i < rowSize; i++){
+			for(int j = 0; j < colSize; j++){
+				if(visited[i][j] == 0 && landscape[i][j] < w){ // if the current point is unvisited and contains water, perform a BFS on it
+					Lake lake = BFS(i, j);
+					lakes.add(lake);
+				}
+				else{
+					visited[i][j] = 1;
+				}
+			}
+		}
+	}
+	
+	// performs a breadth first search to find all adjacent points that are filled with water and unvisited
+	// returns a Lake
+	public static Lake BFS(int i, int j){
+		Lake lake = new Lake(); // create the lake
+		Queue<Point> q = new LinkedList<Point>(); // create queue for BFS
+		Point p = new Point(i, j, landscape[i][j]); // create point
+		visited[i][j] = 1; // mark point as visited
+		lake.addPoint(p); // add point to lake
+		q.offer(p); // enqueue the point
+		
+		// while the queue is not empty
+		while(!q.isEmpty()){
+			p = q.poll(); // dequeue the point
+			
+			// check points for 3 conditions: (1) not out of bounds (2) not visited (3) below water level
+			// only need to check points below and to the right
+			// check point below p
+			if((p.row+1 < rowSize) && (visited[p.row+1][p.col] == 0) && (landscape[p.row+1][p.col] < w)){ 
+				Point newP = new Point(p.row+1, p.col, landscape[p.row+1][p.col]); // create the new point
+				lake.addPoint(newP); // add the new point to the lake
+				visited[newP.row][newP.col] = 1; // mark the new point as visited
+				q.offer(newP); // enqueue the new point
+			}
+			// check point right of p
+			if((p.col+1 < colSize) && (visited[p.row][p.col+1] == 0) && (landscape[p.row][p.col+1] < w)){ 
+				Point newP = new Point(p.row, p.col+1, landscape[p.row][p.col+1]);
+				lake.addPoint(newP);
+				visited[newP.row][newP.col] = 1;
+				q.offer(newP);
+			}
+		}
+		lake.SA = lake.computeSA(); // compute the current lake's surface area
+		lake.vol = lake.computeVol(); // compute the current lake's volume
+		return lake;
+	}
+	
+	// finds lake with largest surface area
+	public static Lake findLargestSALake(){
+		if(lakes.size() == 0) return null; // if there's no lakes, return null
+		int largestSA = 0;
+		Lake largestSALake = lakes.get(0);
+		for(int i = 0; i < lakes.size(); i++){
+			if(lakes.get(i).SA > largestSA){
+				largestSA = lakes.get(i).SA;
+				largestSALake = lakes.get(i);
+			}
+		}
+		return largestSALake;
+	}
+	
+	// finds lake with largest volume
+	public static Lake findLargestVolLake(){
+		if(lakes.size() == 0) return null; // if there's no lakes, return null
+		int largestVol = 0;
+		Lake largestVolLake = lakes.get(0);
+		for(int i = 0; i < lakes.size(); i++){
+			if(lakes.get(i).SA > largestVol){
+				largestVol = lakes.get(i).SA;
+				largestVolLake = lakes.get(i);
+			}
+		}
+		return largestVolLake;
+	}
+	
+	// finds the shortest motorable path between point A and point B using BFS
+	public static void findPath(){
+		// check if the start and end points are above water
+		if(a.elev < w || b.elev < w){
+			System.out.println("Cannot find path, one of these points is underwater!");
+			return;
+		}
+		// clear visited grid for reuse
+		for(int i = 0; i < rowSize; i++){
+			for(int j = 0; j < colSize; j++){
+				visited[i][j] = 0;
+			}
+		}		
+		ArrayList<Point> path = new ArrayList<Point>(); // create an ArrayList for the reverse path
+		Queue<Point> q = new LinkedList<Point>(); // create queue for BFS
+		Point p = a; // point p is starting point
+		visited[p.row][p.col] = 1; // mark point as visited
+		q.offer(p); // enqueue the point
+		
+		// while the queue is not empty
+		while(!q.isEmpty()){
+			p = q.poll(); // dequeue the point
+			// this point is point b! exit
+			if(p.row == b.row && p.col == b.col){
+				break;
+			}
+			// check points for 4 conditions: (1) not out of bounds (2) not visited (3) equal to or above water level (4) gradient less than threshold g
+			// check point above p
+			if((p.row-1 >= 0) && (visited[p.row-1][p.col] == 0) && 
+					(landscape[p.row-1][p.col] >= w) && Math.abs(p.elev - landscape[p.row-1][p.col]) < g){ 
+				Point newP = new Point(p.row-1, p.col, landscape[p.row-1][p.col], p); // create the point, this time keep track of parent pointer
+				visited[newP.row][newP.col] = 1; // mark point as visited
+				q.offer(newP); // enqueue point
+			}
+			// check point below p
+			if((p.row+1 < rowSize) && (visited[p.row+1][p.col] == 0) && 
+					(landscape[p.row+1][p.col] >= w) && Math.abs(p.elev - landscape[p.row+1][p.col]) < g){ 
+				Point newP = new Point(p.row+1, p.col, landscape[p.row+1][p.col], p);
+				visited[newP.row][newP.col] = 1;
+				q.offer(newP);
+			}
+			// check point left of p
+			if((p.col-1 >= 0) && (visited[p.row][p.col-1] == 0) && 
+					(landscape[p.row][p.col-1] >= w) && Math.abs(p.elev - landscape[p.row][p.col-1]) < g){ 
+				Point newP = new Point(p.row, p.col-1, landscape[p.row][p.col-1], p);
+				visited[newP.row][newP.col] = 1;
+				q.offer(newP);
+			}
+			// check point right of p
+			if((p.col+1 < colSize) && (visited[p.row][p.col+1] == 0) && 
+					(landscape[p.row][p.col+1] >= w) && Math.abs(p.elev - landscape[p.row][p.col+1]) < g){ 
+				Point newP = new Point(p.row, p.col+1, landscape[p.row][p.col+1], p);
+				visited[newP.row][newP.col] = 1;
+				q.offer(newP);
+			}
+		}
+		// No path
+		if(p.row != b.row || p.col != b.col){
+			System.out.println("There is no path from A to B");
+		}
+		// There is a path
+		else{
+			// follow each point's parent pointer until parent is null to retrace the path backwards
+			while(p != null){ 
+				path.add(p); // add point to ArrayList path
+				p = p.parent;
+			}
+			System.out.println("This is the shortest motorable path with length " + path.size() + ":");
+			for(int i = path.size()-1; i >= 0; i--){ // iterate through ArrayList path backwards to reconstruct proper path
+				path.get(i).printPoint();
+			}
+		}
+	}
+	
+	// parses test file and populates variables
+	public static void main(String[] args) throws FileNotFoundException{		
+		
+		Scanner scanner = new Scanner(new File("test.txt"));
+		
+		// initialize row size and column size
+		rowSize = scanner.nextInt();
+		colSize = scanner.nextInt();
+		
+		// initialize water level and gradient threshold
+		w = scanner.nextInt(); g = scanner.nextInt();
+		
+		// initialize point A and point B for question 3
+		int aRow = scanner.nextInt(); int aCol = scanner.nextInt();
+		int bRow = scanner.nextInt(); int bCol = scanner.nextInt();
+		a = new Point(aRow, aCol, 0); // temporarily assigns A's elevation to 0
+		b = new Point(bRow, bCol, 0); // temporarily assigns B's elevation to 0
+		
+		// initialize landscape and visited
+		landscape = new int[rowSize][colSize];
+		visited = new int[rowSize][colSize];
+		for(int i = 0; i < rowSize; i++){
+			for(int j = 0; j < colSize; j++){
+				landscape[i][j] = scanner.nextInt();
+				visited[i][j] = 0;
+			}
+		}
+		a.elev = landscape[aRow][aCol];	// reassign A's elevation after parsing grid
+		b.elev = landscape[bRow][bCol]; // reassign B's elevation after parsing grid
+		scanner.close();
+		
+//		Printer.printGrid(landscape); // prints the grid		
+		Printer.printInitialVals(); // print all initial values		
+		findLakes();		
+		Printer.printLakes(lakes); // print all the lakes
+		
+		// print lake with largest surface area
+		Lake lake = findLargestSALake();
+		System.out.println("This lake has the largest surface area of size " + lake.SA + ":");
+		lake.printLake();
+		
+		// print lake with largest volume
+		lake = findLargestVolLake();
+		System.out.println("This lake has the largest volume of size " + lake.vol + ":");
+		lake.printLake();
+		
+		// print the motorable path if possible		
+		findPath();
+	}
+}
---
+++ src/landscape/Printer.java
@@ -0,0 +1,44 @@
+package landscape;
+
+import java.util.ArrayList;
+
+import landscape.Landscape.Lake;
+
+public class Printer {
+	
+	// print grid
+	public static void printGrid(int[][] grid) {		
+		System.out.printf("%-4s", "");
+		for (int i = 0; i < grid[0].length; i++) {
+		    System.out.printf("%-4d", i);
+		}
+		System.out.println();
+		for (int i = 0; i < grid.length; i++) {
+		    System.out.printf("%-4d", i);
+		    for (int j = 0; j < grid[0].length; j++) {
+		        System.out.printf("%-4d", grid[i][j]);
+		    }
+		    System.out.println();
+		}
+	}
+	
+	// print initial values
+	public static void printInitialVals(){
+		System.out.println("Row size: " + Landscape.rowSize + ", Column size: " + Landscape.colSize);
+		System.out.println("Water level W: " + Landscape.w);
+		System.out.println("Gradient threshold G: " + Landscape.g);
+		System.out.print("Point A: "); Landscape.a.printPoint();
+		System.out.print("Point B: "); Landscape.b.printPoint();
+	}
+	
+	// print all the lakes
+	public static void printLakes(ArrayList<Landscape.Lake> lakes){
+		System.out.println("All the lakes: ");
+		for(int i = 0; i < lakes.size(); i++){
+			int lakeNum = i+1;
+			System.out.println("Lake #" + lakeNum);
+			Lake curLake = lakes.get(i);
+			curLake.printLake();
+		}
+	}
+}
